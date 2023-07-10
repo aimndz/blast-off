f9476a01c974d6853a26693cde640e679e3aa8d6
Amiel Ian C. Mendoza <amielmendoza143@gmail.com>
2023-07-10 20:56:17 +0800

    Update content

--- index.js
+++ index.js
@@ -13,105 +13,37 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
-/**********NAV RESPONSIVENESS**********/
 
-/**********CATEGORIES SLIDER**********/
-const carousel = document.querySelector(".carousel"),
-  firstImg = carousel.querySelectorAll("img")[0],
-  arrowIcons = document.querySelectorAll(".wrapper i");
+function searchItems() {
+  // Get the search query
+  const query = document.getElementById("search-input").value.trim();
 
-let isDragStart = false,
-  isDragging = false,
-  prevPageX,
-  prevScrollLeft,
-  positionDiff;
+  if (query !== "") {
+    // Serialize the search query and store it in local storage
+    localStorage.setItem("searchQuery", query);
 
-const showHideIcons = () => {
-  // showing and hiding prev/next icon according to carousel scroll left value
-  let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
-  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-  arrowIcons[1].style.display =
-    carousel.scrollLeft == scrollWidth ? "none" : "block";
-};
-
-arrowIcons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-    // if clicked icon is left, reduce width value from the carousel scroll left else add to it
-    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-    setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
-  });
-});
-
-const autoSlide = () => {
-  // if there is no image left to scroll then return from here
-  if (
-    carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 ||
-    carousel.scrollLeft <= 0
-  )
-    return;
-
-  positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
-  let firstImgWidth = firstImg.clientWidth + 14;
-  // getting difference value that needs to add or reduce from carousel left to take middle img center
-  let valDifference = firstImgWidth - positionDiff;
-
-  if (carousel.scrollLeft > prevScrollLeft) {
-    // if user is scrolling to the right
-    return (carousel.scrollLeft +=
-      positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff);
+    // Redirect the user to the search results page
+    window.location.href = "search-result.html";
   }
-  // if user is scrolling to the left
-  carousel.scrollLeft -=
-    positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-};
+}
 
-const dragStart = (e) => {
-  // updatating global variables value on mouse down event
-  isDragStart = true;
-  prevPageX = e.pageX || e.touches[0].pageX;
-  prevScrollLeft = carousel.scrollLeft;
-};
+updateCartQuantity();
 
-const dragging = (e) => {
-  // scrolling images/carousel to left according to mouse pointer
-  if (!isDragStart) return;
-  e.preventDefault();
-  isDragging = true;
-  carousel.classList.add("dragging");
-  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-  carousel.scrollLeft = prevScrollLeft - positionDiff;
-  showHideIcons();
-};
+function updateCartQuantity() {
+  cartItemsLocal = localStorage.getItem("cartItems");
 
-const dragStop = () => {
-  isDragStart = false;
-  carousel.classList.remove("dragging");
+  const shoppingCart = document.querySelector(".shopping-cart");
+  let style = window.getComputedStyle(shoppingCart, "::after");
+  let contentValue = style.getPropertyValue("content");
+  let numberValue = contentValue.replace(/["'"]/g, "");
 
-  if (!isDragging) return;
-  isDragging = false;
-  autoSlide();
-};
+  if (cartItemsLocal) {
+    let cartItems = JSON.parse(cartItemsLocal);
+    let cartItemsQuantity = cartItems.length;
 
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
+    contentValue = `"${cartItemsQuantity}"`;
+    shoppingCart.style.setProperty("--cart-content", contentValue);
+  }
+}
 
-document.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-
-document.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
-
-/**********CATEGORIES SLIDER**********/
-
-/**********PRELOADER**********/
-window.addEventListener("load", function () {
-  let preloader = document.getElementById("preloader");
-  let video = document.getElementById("preloader-video");
-
-  video.addEventListener("ended", function () {
-    preloader.style.display = "none";
-    document.getElementById("main-content").style.display = "block";
-  });
-});
-/**********PRELOADER**********/
+/**********NAV RESPONSIVENESS**********/
